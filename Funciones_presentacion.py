66ca41f713008a7fe58bb5b27ebfb5a533dc3501
Dazti1 <47644960+Dazti1@users.noreply.github.com>
2022-06-28 12:31:57 -0400

    Update Funciones_presentacion.py

--- Funciones_presentacion.py
+++ Funciones_presentacion.py
@@ -3,3 +3,9 @@ def ponderado(nota,ponderacion):
     ponderacion=ponderacion/100
     nota=nota*ponderacion
     return nota
+
+def nota_eximicion(nota_eximicion,nota_acumulada,ponderacion_acumulada):
+    ponderacion=0
+    ponderacion=1-(ponderacion_acumulada/100)
+    nota_eximicion=(nota_eximicion-nota_acumulada)/ponderacion
+    return nota_eximicion
