9156c425f76206042ac7b4d241dff7a1ef922f49
Dazti1 <47644960+Dazti1@users.noreply.github.com>
2022-06-29 18:07:05 -0400

    Update Funciones_presentacion.py
    funciones para app_presentacion.py

--- Funciones_presentacion.py
+++ Funciones_presentacion.py
@@ -1,11 +1,10 @@
-
 def ponderado(nota,ponderacion):
     ponderacion=ponderacion/100
-    nota=nota*ponderacion
+    nota=round((nota*ponderacion),2)
     return nota
 
 def nota_eximicion(nota_eximicion,nota_acumulada,ponderacion_acumulada):
     ponderacion=0
     ponderacion=1-(ponderacion_acumulada/100)
-    nota_eximicion=(nota_eximicion-nota_acumulada)/ponderacion
+    nota_eximicion=round((((nota_eximicion-nota_acumulada)/ponderacion)+0.05),1) # El +0.05 es para redondear por exceso en el primer decimal
     return nota_eximicion
